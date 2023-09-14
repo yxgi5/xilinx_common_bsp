c43e7a3d6f995e3c40e04ee833ba48a49bd2525d
Andreas Zhang <denglitsch@gmail.com>
2023-09-14 10:40:49 +0800

    update: platform 中断调用方式

--- platform.c
+++ platform.c
@@ -17,19 +17,23 @@
 
  #define UART_BAUD 9600
 #endif
+#if defined (ARMR5) || (__aarch64__) || (__arm__) || (__PPC__)
+#include "xtime_l.h"
+#endif
 
 //#if defined (UDP_UPDATE) && defined (PLATFORM_ZYNQMP)
 #if defined (__UDP_UPDATE_H__) && defined (PLATFORM_ZYNQMP)
 #include "xttcps.h"
-
+#include "arch/cc.h"
 #define ETH_LINK_DETECT_INTERVAL 4
 #define PLATFORM_TIMER_INTR_RATE_HZ (4)
-static XTtcPs TimerInstance;
 static XInterval Interval;
 static u8 Prescaler;
 volatile int TcpFastTmrFlag = 0;
 volatile int TcpSlowTmrFlag = 0;
-#endif // UDP_UPDATE
+XScuGic InterruptController; 	     /* Instance of the Interrupt Controller */
+XTtcPs TimerInstance;
+#endif // __UDP_UPDATE_H__ && PLATFORM_ZYNQMP
 
 void enable_caches(void)
 {
@@ -152,10 +156,27 @@ void platform_setup_timer(void)
 void platform_setup_interrupts(void)
 {
 #if defined (PLATFORM_ZYNQMP)
+	int Status;
+	XScuGic_Config *GicConfig;    /* The configuration parameters of the controller */
+
 	Xil_ExceptionInit();
 
-	XScuGic_DeviceInitialize(XPAR_SCUGIC_SINGLE_DEVICE_ID);
+//	XScuGic_DeviceInitialize(XPAR_SCUGIC_SINGLE_DEVICE_ID);
 
+	/*
+	 * Initialize the interrupt controller driver so that it is ready to
+	 * use.
+	 */
+	GicConfig = XScuGic_LookupConfig(XPAR_SCUGIC_0_DEVICE_ID);
+	if (NULL == GicConfig) {
+		return XST_FAILURE;
+	}
+
+	Status = XScuGic_CfgInitialize(&InterruptController, GicConfig,
+					GicConfig->CpuBaseAddress);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
 	/*
 	 * Connect the interrupt controller interrupt handler to the hardware
 	 * interrupt handling logic in the processor.
@@ -163,18 +184,31 @@ void platform_setup_interrupts(void)
 	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_IRQ_INT,
 			(Xil_ExceptionHandler)XScuGic_DeviceInterruptHandler,
 			(void *)XPAR_SCUGIC_SINGLE_DEVICE_ID);
+//	/*
+//	 * Connect the device driver handler that will be called when an
+//	 * interrupt for the device occurs, the handler defined above performs
+//	 * the specific interrupt processing for the device.
+//	 */
+//	XScuGic_RegisterHandler(XPAR_SCUGIC_0_CPU_BASEADDR, XPAR_XTTCPS_0_INTR,
+//					(Xil_ExceptionHandler)timer_callback,
+//					(void *)&TimerInstance);
+//	/*
+//	 * Enable the interrupt for scu timer.
+//	 */
+//	XScuGic_EnableIntr(XPAR_SCUGIC_0_DIST_BASEADDR, XPAR_XTTCPS_0_INTR);
+
 	/*
-	 * Connect the device driver handler that will be called when an
-	 * interrupt for the device occurs, the handler defined above performs
-	 * the specific interrupt processing for the device.
+	 * Connect a device driver handler that will be called when an
+	 * interrupt for the device occurs, the device driver handler performs
+	 * the specific interrupt processing for the device
 	 */
-	XScuGic_RegisterHandler(XPAR_SCUGIC_0_CPU_BASEADDR, XPAR_XTTCPS_0_INTR,
-					(Xil_ExceptionHandler)timer_callback,
-					(void *)&TimerInstance);
-	/*
-	 * Enable the interrupt for scu timer.
-	 */
-	XScuGic_EnableIntr(XPAR_SCUGIC_0_DIST_BASEADDR, XPAR_XTTCPS_0_INTR);
+	Status = XScuGic_Connect(&InterruptController, XPAR_XTTCPS_0_INTR,
+			   (Xil_ExceptionHandler)timer_callback,
+			   (void *)&TimerInstance);
+
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
 
 	return;
 #endif // PLATFORM_ZYNQMP
@@ -187,12 +221,48 @@ void platform_enable_interrupts()
 	 * Enable non-critical exceptions.
 	 */
 	Xil_ExceptionEnableMask(XIL_EXCEPTION_IRQ);
-	XScuGic_EnableIntr(XPAR_SCUGIC_0_DIST_BASEADDR, XPAR_XTTCPS_0_INTR);
+//	XScuGic_EnableIntr(XPAR_SCUGIC_0_DIST_BASEADDR, XPAR_XTTCPS_0_INTR);
+//	XTtcPs_EnableInterrupts(&TimerInstance, XTTCPS_IXR_INTERVAL_MASK);
+
+	/*
+	 * Enable the interrupt for the Timer counter
+	 */
+	XScuGic_Enable(&InterruptController, XPAR_XTTCPS_0_INTR);
+
+	/*
+	 * Enable the interrupts for the tick timer/counter
+	 * We only care about the interval timeout.
+	 */
 	XTtcPs_EnableInterrupts(&TimerInstance, XTTCPS_IXR_INTERVAL_MASK);
+
 	XTtcPs_Start(&TimerInstance);
 	return;
 #endif // PLATFORM_ZYNQMP
 }
+
+#if defined (ARMR5) || (__aarch64__) || (__arm__)
+u64_t get_time_ms()
+{
+#define COUNTS_PER_MILLI_SECOND (COUNTS_PER_SECOND/1000)
+
+#if defined(ARMR5)
+	XTime tCur = 0;
+	static XTime tlast = 0, tHigh = 0;
+	u64_t time;
+	XTime_GetTime(&tCur);
+	if (tCur < tlast)
+		tHigh++;
+	tlast = tCur;
+	time = (((u64_t) tHigh) << 32U) | (u64_t)tCur;
+	return (time/COUNTS_PER_MILLI_SECOND);
+#else // (__aarch64__) || (__arm__)
+	XTime tCur = 0;
+	XTime_GetTime(&tCur);
+	return (tCur/COUNTS_PER_MILLI_SECOND);
+#endif
+}
+#endif
+
 #endif // UDP_UPDATE
 
 void init_platform(void)
