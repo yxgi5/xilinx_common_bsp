ffbc275e0b0b58521f81fbdccd672d32f2a13549
Andreas Zhang <denglitsch@gmail.com>
2023-09-14 12:14:27 +0800

    update: udp_update

--- platform.c
+++ platform.c
@@ -13,27 +13,61 @@
 //#include "psu_init.h"
 //#endif 
 #ifdef STDOUT_IS_16550
- #include "xuartns550_l.h"
-
- #define UART_BAUD 9600
+	#include "xuartns550_l.h"
+	#define UART_BAUD 9600
 #endif
+
 #if defined (ARMR5) || (__aarch64__) || (__arm__) || (__PPC__)
-#include "xtime_l.h"
+	#include "xtime_l.h"
 #endif
 
-//#if defined (UDP_UPDATE) && defined (PLATFORM_ZYNQMP)
-#if defined (__UDP_UPDATE_H__) && defined (PLATFORM_ZYNQMP)
-#include "xttcps.h"
-#include "arch/cc.h"
-#define ETH_LINK_DETECT_INTERVAL 4
-#define PLATFORM_TIMER_INTR_RATE_HZ (4)
-static XInterval Interval;
-static u8 Prescaler;
-volatile int TcpFastTmrFlag = 0;
-volatile int TcpSlowTmrFlag = 0;
-XScuGic InterruptController; 	     /* Instance of the Interrupt Controller */
-XTtcPs TimerInstance;
-#endif // __UDP_UPDATE_H__ && PLATFORM_ZYNQMP
+#if defined (__UDP_UPDATE_H__)
+	#if defined (PLATFORM_ZYNQ) || defined (PLATFORM_ZYNQMP)
+		#include "arch/cc.h"
+//		#define INTC_DEVICE_ID			XPAR_SCUGIC_SINGLE_DEVICE_ID
+		#define INTC_DEVICE_ID			XPAR_SCUGIC_0_DEVICE_ID
+		#define INTC_BASE_ADDR 			XPAR_SCUGIC_0_CPU_BASEADDR
+		#define INTC_DIST_BASE_ADDR 	XPAR_SCUGIC_0_DIST_BASEADDR
+		/* Platform timer is calibrated for 250 ms, so kept interval value 4 to call
+		 * eth_link_detect() at every one second
+		 */
+		#define ETH_LINK_DETECT_INTERVAL (4)
+		void tcp_fasttmr(void);
+		void tcp_slowtmr(void);
+		volatile int TcpFastTmrFlag = 0;
+		volatile int TcpSlowTmrFlag = 0;
+		XScuGic InterruptController; 	     /* Instance of the Interrupt Controller */
+		#if LWIP_DHCP==1
+			volatile int dhcp_timoutcntr = 24;
+			void dhcp_fine_tmr();
+			void dhcp_coarse_tmr();
+		#endif // LWIP_DHCP
+	#endif // PLATFORM_ZYNQ || PLATFORM_ZYNQMP
+
+	#if defined (PLATFORM_ZYNQMP)
+		#include "xttcps.h"
+		#define TIMER_DEVICE_ID			XPAR_XTTCPS_0_DEVICE_ID
+		#define TIMER_IRPT_INTR 		XPAR_XTTCPS_0_INTR
+		#define PLATFORM_TIMER_INTR_RATE_HZ (4)
+		static XInterval Interval;
+		static u8 Prescaler;
+		XTtcPs TimerInstance;
+		//extern struct netif *echo_netif;
+	#endif // PLATFORM_ZYNQMP
+
+	#if defined (PLATFORM_ZYNQ)
+		#include "xscutimer.h"
+		#define TIMER_DEVICE_ID			XPAR_SCUTIMER_DEVICE_ID
+		#define TIMER_IRPT_INTR			XPAR_SCUTIMER_INTR
+		#define RESET_RX_CNTR_LIMIT	(400)
+		#ifndef USE_SOFTETH_ON_ZYNQ
+			static int ResetRxCntr = 0;
+		#endif
+		XScuTimer TimerInstance;
+		//extern struct netif *echo_netif;
+	#endif // PLATFORM_ZYNQ
+
+#endif // __UDP_UPDATE_H__
 
 void enable_caches(void)
 {
@@ -82,6 +116,64 @@ void init_uart(void)
 }
 
 #if defined (UDP_UPDATE)
+
+#if defined (PLATFORM_ZYNQ)
+void timer_callback(XScuTimer * TimerInstance)
+{
+	static int DetectEthLinkStatus = 0;
+	/* we need to call tcp_fasttmr & tcp_slowtmr at intervals specified
+	 * by lwIP. It is not important that the timing is absoluetly accurate.
+	 */
+	static int odd = 1;
+#if LWIP_DHCP==1
+    static int dhcp_timer = 0;
+#endif
+	DetectEthLinkStatus++;
+	 TcpFastTmrFlag = 1;
+
+	odd = !odd;
+#ifndef USE_SOFTETH_ON_ZYNQ
+	ResetRxCntr++;
+#endif
+	if (odd) {
+#if LWIP_DHCP==1
+		dhcp_timer++;
+		dhcp_timoutcntr--;
+#endif
+		TcpSlowTmrFlag = 1;
+#if LWIP_DHCP==1
+		dhcp_fine_tmr();
+		if (dhcp_timer >= 120) {
+			dhcp_coarse_tmr();
+			dhcp_timer = 0;
+		}
+#endif
+	}
+
+	/* For providing an SW alternative for the SI #692601. Under heavy
+	 * Rx traffic if at some point the Rx path becomes unresponsive, the
+	 * following API call will ensures a SW reset of the Rx path. The
+	 * API xemacpsif_resetrx_on_no_rxdata is called every 100 milliseconds.
+	 * This ensures that if the above HW bug is hit, in the worst case,
+	 * the Rx path cannot become unresponsive for more than 100
+	 * milliseconds.
+	 */
+#ifndef USE_SOFTETH_ON_ZYNQ
+	if (ResetRxCntr >= RESET_RX_CNTR_LIMIT) {
+		xemacpsif_resetrx_on_no_rxdata(echo_netif);
+		ResetRxCntr = 0;
+	}
+#endif
+	/* For detecting Ethernet phy link status periodically */
+	if (DetectEthLinkStatus == ETH_LINK_DETECT_INTERVAL) {
+		eth_link_detect(echo_netif);
+		DetectEthLinkStatus = 0;
+	}
+
+	XScuTimer_ClearInterruptStatus(TimerInstance);
+}
+#endif // PLATFORM_ZYNQ
+
 #if defined (PLATFORM_ZYNQMP)
 void platform_clear_interrupt( XTtcPs * TimerInstance )
 {
@@ -132,13 +224,45 @@ void timer_callback(XTtcPs * TimerInstance)
 
 void platform_setup_timer(void)
 {
+#if defined (PLATFORM_ZYNQ)
+	int Status = XST_SUCCESS;
+	XScuTimer_Config *ConfigPtr;
+	int TimerLoadValue = 0;
+
+	ConfigPtr = XScuTimer_LookupConfig(TIMER_DEVICE_ID);
+	Status = XScuTimer_CfgInitialize(&TimerInstance, ConfigPtr,
+			ConfigPtr->BaseAddr);
+	if (Status != XST_SUCCESS) {
+
+		xil_printf("In %s: Scutimer Cfg initialization failed...\r\n",
+		__func__);
+		return;
+	}
+
+	Status = XScuTimer_SelfTest(&TimerInstance);
+	if (Status != XST_SUCCESS) {
+		xil_printf("In %s: Scutimer Self test failed...\r\n",
+		__func__);
+		return;
+
+	}
+
+	XScuTimer_EnableAutoReload(&TimerInstance);
+	/*
+	 * Set for 250 milli seconds timeout.
+	 */
+	TimerLoadValue = XPAR_CPU_CORTEXA9_0_CPU_CLK_FREQ_HZ / 8;
+
+	XScuTimer_LoadTimer(&TimerInstance, TimerLoadValue);
+#endif // PLATFORM_ZYNQ
+
 #if defined (PLATFORM_ZYNQMP)
 	int Status;
 	XTtcPs * Timer = &TimerInstance;
 	XTtcPs_Config *Config;
 
 
-	Config = XTtcPs_LookupConfig(XPAR_XTTCPS_0_DEVICE_ID);
+	Config = XTtcPs_LookupConfig(TIMER_DEVICE_ID);
 
 	Status = XTtcPs_CfgInitialize(Timer, Config, Config->BaseAddress);
 	if (Status != XST_SUCCESS) {
@@ -155,19 +279,19 @@ void platform_setup_timer(void)
 
 void platform_setup_interrupts(void)
 {
-#if defined (PLATFORM_ZYNQMP)
+#if defined (PLATFORM_ZYNQMP)  || defined (PLATFORM_ZYNQ)
 	int Status;
 	XScuGic_Config *GicConfig;    /* The configuration parameters of the controller */
 
 	Xil_ExceptionInit();
 
-//	XScuGic_DeviceInitialize(XPAR_SCUGIC_SINGLE_DEVICE_ID);
+//	XScuGic_DeviceInitialize(INTC_DEVICE_ID);
 
 	/*
 	 * Initialize the interrupt controller driver so that it is ready to
 	 * use.
 	 */
-	GicConfig = XScuGic_LookupConfig(XPAR_SCUGIC_0_DEVICE_ID);
+	GicConfig = XScuGic_LookupConfig(INTC_DEVICE_ID);
 	if (NULL == GicConfig) {
 		return XST_FAILURE;
 	}
@@ -183,26 +307,26 @@ void platform_setup_interrupts(void)
 	 */
 	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_IRQ_INT,
 			(Xil_ExceptionHandler)XScuGic_DeviceInterruptHandler,
-			(void *)XPAR_SCUGIC_SINGLE_DEVICE_ID);
+			(void *)INTC_DEVICE_ID);
 //	/*
 //	 * Connect the device driver handler that will be called when an
 //	 * interrupt for the device occurs, the handler defined above performs
 //	 * the specific interrupt processing for the device.
 //	 */
-//	XScuGic_RegisterHandler(XPAR_SCUGIC_0_CPU_BASEADDR, XPAR_XTTCPS_0_INTR,
+//	XScuGic_RegisterHandler(INTC_BASE_ADDR, TIMER_IRPT_INTR,
 //					(Xil_ExceptionHandler)timer_callback,
 //					(void *)&TimerInstance);
 //	/*
 //	 * Enable the interrupt for scu timer.
 //	 */
-//	XScuGic_EnableIntr(XPAR_SCUGIC_0_DIST_BASEADDR, XPAR_XTTCPS_0_INTR);
+//	XScuGic_EnableIntr(INTC_DIST_BASE_ADDR, TIMER_IRPT_INTR);
 
 	/*
 	 * Connect a device driver handler that will be called when an
 	 * interrupt for the device occurs, the device driver handler performs
 	 * the specific interrupt processing for the device
 	 */
-	Status = XScuGic_Connect(&InterruptController, XPAR_XTTCPS_0_INTR,
+	Status = XScuGic_Connect(&InterruptController, TIMER_IRPT_INTR,
 			   (Xil_ExceptionHandler)timer_callback,
 			   (void *)&TimerInstance);
 
@@ -216,6 +340,15 @@ void platform_setup_interrupts(void)
 
 void platform_enable_interrupts()
 {
+#if defined (PLATFORM_ZYNQ)
+	/*
+	 * Enable non-critical exceptions.
+	 */
+	Xil_ExceptionEnableMask(XIL_EXCEPTION_IRQ);
+	XScuGic_Enable(&InterruptController, TIMER_IRPT_INTR);
+	XScuTimer_EnableInterrupt(&TimerInstance);
+	XScuTimer_Start(&TimerInstance);
+#endif // PLATFORM_ZYNQ
 #if defined (PLATFORM_ZYNQMP)
 	/*
 	 * Enable non-critical exceptions.
@@ -227,7 +360,7 @@ void platform_enable_interrupts()
 	/*
 	 * Enable the interrupt for the Timer counter
 	 */
-	XScuGic_Enable(&InterruptController, XPAR_XTTCPS_0_INTR);
+	XScuGic_Enable(&InterruptController, TIMER_IRPT_INTR);
 
 	/*
 	 * Enable the interrupts for the tick timer/counter
